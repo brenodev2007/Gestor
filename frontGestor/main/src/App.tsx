48af9beefd0ed4dbe2290793eaacb1dcd7d736a4
Breno <breno.soriani@gmail.com>
2025-09-24 16:50:24 -0300

    Página de login

--- frontGestor/main/src/App.tsx
+++ frontGestor/main/src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/components/login";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        {/* Tela de login */}
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
