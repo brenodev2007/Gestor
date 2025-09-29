b1ae02220cfd45f42536c93a3b65ebba460c033d
breno.soriani <breno.soriani@gmail.com>
2025-09-29 18:10:55 +0000

    Mudando estilos

--- controlfy/src/pages/Login.tsx
+++ controlfy/src/pages/Login.tsx
@@ -6,13 +6,13 @@ import { Link } from "react-router-dom";
 
 export function Login() {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
+            <h1 className="text-3xl font-bold text-emerald-500">Control-Fy</h1>
             <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
+              Faça o login para continuar
             </p>
           </div>
           <div className="grid gap-4">
@@ -23,43 +23,42 @@ export function Login() {
                 type="email"
                 placeholder="m@example.com"
                 required
+                className="border-emerald-500"
               />
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
+                <Label htmlFor="password">Senha</Label>
                 <Link
                   to="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
+                  className="ml-auto inline-block text-sm text-emerald-500 underline"
                 >
-                  Forgot your password?
+                  Esqueceu sua senha?
                 </Link>
               </div>
-              <Input id="password" type="password" required />
+              <Input id="password" type="password" required className="border-emerald-500" />
             </div>
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full bg-emerald-500 hover:bg-emerald-600 text-white">
               Login
             </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
+            <Button variant="outline" className="w-full border-emerald-500 text-emerald-500 hover:bg-emerald-500 hover:text-white">
+              Login com Google
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
-            Don't have an account?{" "}
-            <Link to="/sign-up" className="underline">
-              Sign up
+            Não tem uma conta?{" "}
+            <Link to="/sign-up" className="underline text-emerald-500">
+              Cadastre-se
             </Link>
           </div>
         </div>
       </div>
       <div className="hidden bg-muted lg:block">
-        {/* <Image
-          src="/placeholder.svg"
+        <img
+          src="https://i.imgur.com/Jvh1O2j.png"
           alt="Image"
-          width="1920"
-          height="1080"
           className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        /> */}
+        />
       </div>
     </div>
   );
--- controlfy/src/pages/SignUp.tsx
+++ controlfy/src/pages/SignUp.tsx
@@ -6,50 +6,60 @@ import { Link } from "react-router-dom";
 
 export function SignUp() {
   return (
-    <div className="flex items-center justify-center py-12">
-      <div className="mx-auto grid w-[350px] gap-6">
-        <div className="grid gap-2 text-center">
-          <h1 className="text-3xl font-bold">Sign Up</h1>
-          <p className="text-balance text-muted-foreground">
-            Enter your information to create an account
-          </p>
-        </div>
-        <div className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="first-name">First name</Label>
-              <Input id="first-name" placeholder="Max" required />
+    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
+         <div className="hidden bg-muted lg:block">
+        <img
+          src="https://i.imgur.com/Jvh1O2j.png"
+          alt="Image"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold text-emerald-500">Control-Fy</h1>
+            <p className="text-balance text-muted-foreground">
+              Insira suas informações para criar uma conta
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="first-name">Primeiro nome</Label>
+                <Input id="first-name" placeholder="Max" required className="border-emerald-500" />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="last-name">Último nome</Label>
+                <Input id="last-name" placeholder="Robinson" required className="border-emerald-500" />
+              </div>
             </div>
             <div className="grid gap-2">
-              <Label htmlFor="last-name">Last name</Label>
-              <Input id="last-name" placeholder="Robinson" required />
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                className="border-emerald-500"
+              />
             </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Senha</Label>
+              <Input id="password" type="password" className="border-emerald-500"/>
+            </div>
+            <Button type="submit" className="w-full bg-emerald-500 hover:bg-emerald-600 text-white">
+              Criar conta
+            </Button>
+            <Button variant="outline" className="w-full border-emerald-500 text-emerald-500 hover:bg-emerald-500 hover:text-white">
+              Cadastre-se com o Google
+            </Button>
           </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
+          <div className="mt-4 text-center text-sm">
+            Já tem uma conta?{" "}
+            <Link to="/login" className="underline text-emerald-500">
+              Login
+            </Link>
           </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-          <Button type="submit" className="w-full">
-            Create an account
-          </Button>
-          <Button variant="outline" className="w-full">
-            Sign up with Google
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Already have an account?{" "}
-          <Link to="/login" className="underline">
-            Sign in
-          </Link>
         </div>
       </div>
     </div>
